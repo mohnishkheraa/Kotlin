1833db99e9932dd4952fed5a47c0ff4036cbb471
mohnishkheraa <mohnishkhera@ltts.com>
2021-04-06 10:22:24 +0530

    a

---
+++ KotProject/app/build/generated/source/buildConfig/androidTest/debug/com/ltts/kotproject/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ltts.kotproject.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ltts.kotproject.test";
+  public static final String BUILD_TYPE = "debug";
+}
